38e55e8e6295d9211eee9eff5df52ee0bf3e3a0a
xushuhui <474497097@qq.com>
2019-09-12 11:54:42 +0800

    add linked list

--- linked_list/linked_list.go
+++ linked_list/linked_list.go
@@ -6,3 +6,59 @@
  * 博客: https://www.phpst.cn
  */
 package linked_list
+
+import "fmt"
+
+type Node struct {
+	e    interface{}
+	node *Node
+}
+
+func (n *Node) String() string {
+	return fmt.Sprint(n.e)
+}
+
+type LinkedList struct {
+	head *Node
+	size int
+}
+
+func (this *LinkedList) GetSize() {
+
+}
+func (this *LinkedList) IsEmpty() {
+
+}
+func (this *LinkedList) AddFirst() {
+
+}
+func (this *LinkedList) Add() {
+
+}
+func (this *LinkedList) AddLast() {
+
+}
+func (this *LinkedList) Remove() {
+
+}
+func (this *LinkedList) RemoveLast() {
+
+}
+func (this *LinkedList) RemoveFirst() {
+
+}
+func (this *LinkedList) Get() {
+
+}
+func (this *LinkedList) GetFirst() {
+
+}
+func (this *LinkedList) GetLast() {
+
+}
+func (this *LinkedList) Contains() {
+
+}
+func (this *LinkedList) Set() {
+
+}
