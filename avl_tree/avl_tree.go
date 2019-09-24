31434b4f154810defca47503e0fc17e5fa681566
xushuhui <474497097@qq.com>
2019-09-24 11:01:37 +0800

    add file

--- avl_tree/avl_tree.go
+++ avl_tree/avl_tree.go
@@ -5,4 +5,4 @@
  * Email: xushuhui@qq.com
  * 博客: https://www.phpst.cn
  */
- package union_find
+ package avl_tree
--- red_black_tree/rb_tree_test.go
+++ red_black_tree/rb_tree_test.go
@@ -5,4 +5,4 @@
  * Email: xushuhui@qq.com
  * 博客: https://www.phpst.cn
  */
- package union_find
+ package red_black_tree
