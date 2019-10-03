6531ba01c6e6145e63bdf2e95ab0b3acc0e39525
xushuhui <474497097@qq.com>
2019-10-03 20:55:30 +0800

    update

--- segment_tree/segment_tree.go
+++ segment_tree/segment_tree.go
@@ -7,3 +7,49 @@
  */
 package segment_tree
 
+func buildSegmentTree(treeIndex,left,right int){
+
+}
+func GetSize(){
+
+}
+func Get(index int)int{
+
+}
+func leftChild(index int){
+
+}
+func rightChild(index int){
+	
+}
+func Query(queryL,queryR int){
+
+}
+func query(treeIndex,left,right int){
+
+}
+func Set(index int,e interface{}){
+
+}
+func set(treeIndex,left,right index int,e interface{}){
+
+}
+func (this *Array) String() string {
+	buffer := bytes.Buffer{}
+
+	buffer.WriteString("[")
+	for i := 0; i < len(this.tree); i++ {
+		if this.tree[i] != nil {
+			buffer.WriteString(fmt.Sprint(this.tree[i]))
+		} else {
+			buffer.WriteString("nil")
+		}
+
+		if i != len(this.tree)-1 {
+			buffer.WriteString(", ")
+		}
+	}
+	buffer.WriteString("]")
+
+	return buffer.String()
+}
