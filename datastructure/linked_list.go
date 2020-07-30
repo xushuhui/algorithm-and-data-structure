8918815d605d279cef3b1dd01b64543a9323443f
xushuhui <474497097@qq.com>
2020-07-30 16:29:20 +0800

    update

--- datastructure/linked_list.go
+++ datastructure/linked_list.go
@@ -38,8 +38,7 @@ func (l *LinkedList) Add(index int, e interface{}) {
 	for i := 0; i < index; i++ {
 		prev = prev.next
 	}
-	//prev.next = NewLinkedListNode(e, prev.next)
-	prev.next = &LinkedListNode{e, prev.next}
+	prev.next = NewLinkedListNode(e, prev.next)
 	l.size++
 }
 func (l *LinkedList) GetSize() int {
@@ -52,7 +51,92 @@ func (l *LinkedList) AddFirst(e interface{}) {
 	l.Add(0, e)
 }
 func (l *LinkedList) AddLast(e interface{}) {
-	l.Add(l.size-1, e)
+	l.Add(l.size, e)
+}
+func (l *LinkedList) Remove(index int) interface{} {
+	if index < 0 || index >= l.GetSize() {
+		panic("invalid index")
+	}
+	prev := l.dummyHead
+	for i := 0; i < index; i++ {
+		prev = prev.next
+	}
+	retNode := prev.next
+	prev.next = retNode.next
+	retNode.next = nil
+	l.size--
+	return retNode.e
+}
+func (l *LinkedList) RemoveTest(index int) {
+	if index < 0 || index >= l.GetSize() {
+		panic("invalid index")
+	}
+	prev := l.dummyHead
+	for i := 0; i < index; i++ {
+		prev = prev.next
+	}
+	retNode := prev.next
+	retNode2 := prev.next.next
+	prev.next = retNode.next.next
+	retNode.next = nil
+	retNode2.next = nil
+	l.size = l.size - 2
+
+}
+func (l *LinkedList) RemoveLast() interface{} {
+	return l.Remove(l.size - 1)
+}
+func (l *LinkedList) RemoveFirst() interface{} {
+	return l.Remove(0)
+}
+func (l *LinkedList) RemoveElement(e interface{}) {
+	prev := l.dummyHead
+	for prev.next != nil {
+		if prev.next.e == e {
+			delNode := prev.next
+			prev.next = delNode.next
+			delNode.next = nil
+			l.size--
+			break
+		}
+		prev = prev.next
+	}
+}
+func (l *LinkedList) Get(index int) interface{} {
+	if index < 0 || index >= l.GetSize() {
+		panic("invalid index")
+	}
+	current := l.dummyHead.next
+	for i := 0; i < index; i++ {
+		current = current.next
+	}
+	return current.e
+}
+func (l *LinkedList) GetFirst() interface{} {
+	return l.Get(0)
+}
+func (l *LinkedList) GetLast() interface{} {
+	return l.Get(l.size - 1)
+}
+func (l *LinkedList) Set(index int, e interface{}) {
+	if index < 0 || index >= l.GetSize() {
+		panic("invalid index")
+	}
+	current := l.dummyHead.next
+	for i := 0; i < index; i++ {
+		current = current.next
+	}
+	current.e = e
+}
+func (l *LinkedList) Contains(e interface{}) bool {
+	current := l.dummyHead
+	for current.next != nil {
+		if current.e == e {
+			return true
+		}
+		current = current.next
+	}
+	return false
 }
 func (l *LinkedList) String() string {
 	buffer := bytes.Buffer{}
--- test/linked_list_test.go
+++ test/linked_list_test.go
@@ -14,7 +14,22 @@ import (
 
 func TestLinkedList(t *testing.T) {
 	l := datastructure.NewLinkedList()
-	l.AddFirst(1)
-
+	for i := 0; i < 5; i++ {
+		l.AddLast(i)
+	}
 	t.Log(l)
+	l.RemoveTest(3)
+	t.Log(l)
+	//l.AddFirst(1)
+	//l.AddLast(2)
+	//t.Log(l)
+	//l.RemoveFirst()
+	//t.Log(l.GetSize())
+	//val := l.Get(0)
+	//t.Log(val)
+	//l.Set(1,5)
+	//t.Log(l)
+	//l.RemoveElement(2)
+	//t.Log(l)
+
 }
